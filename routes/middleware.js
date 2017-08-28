e87466f397f726e88cad188347d035bc01b0a36e
Justin Bellero <jpbellero@gmail.com>
2017-08-28 14:15:38 -0400

    moves template components to views/components

--- routes/middleware.js
+++ routes/middleware.js
@@ -22,8 +22,8 @@ const year = new Date();
 exports.initLocals = function (req, res, next) {
 	res.locals.navLinks = [
 		{ label: 'Home', key: 'home', href: '/' },
-		{ label: 'Blog', key: 'blog', href: '/blog' },
-		{ label: 'Gallery', key: 'gallery', href: '/gallery' },
+		// { label: 'Blog', key: 'blog', href: '/blog' },
+		// { label: 'Gallery', key: 'gallery', href: '/gallery' },
 		{ label: 'Contact', key: 'contact', href: '/contact' },
 	];
 	res.locals.copyright = year.getFullYear();
