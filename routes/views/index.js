b8ab72355086b8a33426fc2c582e1fb0e2d2aa98
Justin Bellero <jpbellero@gmail.com>
2017-09-05 12:45:06 -0400

    finishes contact form modification

--- routes/views/index.js
+++ routes/views/index.js
@@ -2,13 +2,20 @@ var keystone = require('keystone');
 
 exports = module.exports = function (req, res) {
 
-	var view = new keystone.View(req, res);
-	var locals = res.locals;
+  var view = new keystone.View(req, res);
+  var locals = res.locals;
 
-	// locals.section is used to set the currently selected
-	// item in the header navigation.
-	locals.section = 'home';
+  // locals.section is used to set the currently selected
+  // item in the header navigation.
+  locals.section = 'home';
 
-	// Render the view
-	view.render('index');
+  // fix scroll-efx by updating href of about link
+  var about = locals.navLinks[1];
+  about.href = '#about';
+
+  var tech = { label: 'Technology', key: 'tech', href: '#technology' };
+  locals.navLinks.splice(2, 0, tech);
+
+  // Render the view
+  view.render('index');
 };
